6e0eb7fc0ecb10180adeec6bb463026ba7e6a778
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-01-27 09:21:51 +0000

    python修改。复合最新运算方法

--- trunk/Lite/src/python/org/xidea/lite/Expression.py
+++ trunk/Lite/src/python/org/xidea/lite/Expression.py
@@ -27,7 +27,7 @@ OP_QUESTION_SELECT = (7<<2) +2;#:;
 
 OP_GET_PROP = (8<<2) +2;#.;
 OP_STATIC_GET_PROP = (9<<2) +1;#.#;
-	
+    
 OP_LT = (0xA<<2) +2;#<;
 OP_GT = (0xB<<2) +2;#>;
 OP_LTEQ = (0xC<<2) +2;#<=;
@@ -35,22 +35,21 @@ OP_GTEQ = (0xD<<2) +2;#>=;
 OP_EQ = (0xE<<2) +2;#==;
 OP_NOTEQ = (0xF<<2) +2;#!=;
 OP_AND = (0x10<<2) +2;#&&;
-OP_OR = (0x12<<2) +2;#||;
-	
-	
+OP_OR = (0x11<<2) +2;#||;
+    
+    
 
-OP_NOT = (0x13<<2) +1;#!;
-OP_POS = (0x14<<2) +1;#+;
-OP_NEG = (0x15<<2) +1;#-;
+OP_NOT = (0x12<<2) +1;#!;
+OP_POS = (0x13<<2) +1;#+;
+OP_NEG = (0x14<<2) +1;#-;
 
-OP_GET_METHOD = (0x16<<2) +2;#.();
-OP_INVOKE_METHOD = (0x17<<2) +2;#()
+OP_INVOKE_METHOD = (0x15<<2) +2;#()
 
 
 #map_join
-OP_PARAM_JOIN = (0x18<<2) +2;#,
+OP_PARAM_JOIN = (0x16<<2) +2;#,
 #map,push
-OP_MAP_PUSH = (0x19<<2) +2;#:,
+OP_MAP_PUSH = (0x17<<2) +2;#:,
 
 globalMap = {
     "test":lambda x:x*3
@@ -67,37 +66,62 @@ def evaluate(tokens, context):
 def _evaluate(stack, tokens, context):
     for item in tokens:
         if isinstance(item,list):
-	        type = item[0];
-	        if type > 3:
-	            arg2 = None
-	            arg1 = None
-	            length = type & 3
-	            if length > 1:
-	                arg2 = stack.pop()
-	                arg1 = stack.pop()
-	            else:
-	                if (length == 1):
-	                    arg1 = stack.pop()
-	            if type == OP_PARAM_JOIN:
-	                arg1.append(arg2)
-	                stack.append(arg1)
-	            elif type == OP_MAP_PUSH:
-	                arg1[item[1]]= arg2;
-	                stack.append(arg1)
-	            else:
-	                result = compute(item, arg1, arg2)
-	                if isinstance(result, (LazyToken)):
-	                    _evaluate(stack, result.children, context)
-	                else:
-	                    stack.append(result)
-	        else:
-	            stack.append(getTokenValue(context, item))
-	    
+            type = item[0];
+            if type > 3:
+                arg2 = None
+                arg1 = None
+                length = type & 3
+                if length > 1:
+                    arg2 = stack.pop()
+                    arg1 = stack.pop()
+                elif length == 1:
+                    arg1 = stack.pop();
+                result = compute(item, arg1, arg2)
+                if isinstance(result, (LazyToken)):
+                    _evaluate(stack, result.children, context)
+                else:
+                    stack.append(result);
+            else:
+                stack.append(getTokenValue(context, item));
         else:
             stack.append(item)
 def compute(op,arg1,arg2):
     type = op[0];
-    if type == OP_NOT:
+    """
+    if type == OP_GET_METHOD:
+        #TODO...
+        if(isinstance(arg1,dict ) and arg2 in arg1):
+            return lambda *args:apply(arg1[arg2],args);
+        else:
+            return lambda *args:apply(getattr(arg1,arg2),args);
+    """
+
+    if type == OP_INVOKE_METHOD:
+        if(isinstance(arg1,PropertyValue )):
+            base = arg1.base;
+            name = arg1.name;
+            if(isinstance(arg1,dict ) and name in base):
+                return apply(base[name],arg2);
+            else:
+                return apply(getattr(base,name),arg2);
+        else:
+            return apply(arg1,arg2);
+    if(isinstance(arg1,PropertyValue)):
+        arg1 = arg1.base[arg1.name];
+    if(isinstance(arg2,PropertyValue)):
+        arg2 = arg2.base[arg2.name];
+        
+    if type == OP_STATIC_GET_PROP:
+        return PropertyValue(arg1,op[1]);
+    elif type == OP_GET_PROP:
+        return PropertyValue(arg1,arg2);
+    elif type == OP_PARAM_JOIN:
+        arg1.append(arg2)
+        return arg1;
+    elif type == OP_MAP_PUSH:
+        arg1[item[1]]= arg2;
+        return arg1;
+    elif type == OP_NOT:
         return not arg1;
     elif type == OP_POS:
         return +arg1;
@@ -143,18 +167,6 @@ def compute(op,arg1,arg2):
             return arg2;
         else:
             return arg1;
-    elif type == OP_GET_PROP:
-        return arg1[arg2];
-    elif type == OP_STATIC_GET_PROP:
-        return arg1[op[1]];
-    elif type == OP_GET_METHOD:
-        #TODO...
-        if(isinstance(arg1,dict ) and arg2 in arg1):
-            return lambda *args:apply(arg1[arg2],args);
-        else:
-            return lambda *args:apply(getattr(arg1,arg2),args);
-    elif type == OP_INVOKE_METHOD:
-        return apply(arg1,arg2);
     elif type == OP_PARAM_JOIN:
         arg1.append(arg2);
         return arg1;
@@ -183,15 +195,12 @@ def getTokenValue(context, item):
         return LazyToken(item[1])
 class LazyToken:
     children = 0;
-    def __init__(self, el):
+    def __init__(self, children):
         self.children = children
 
-class Test:
-    test2=1
-    def test(self,x):
-        return x*2;
-    def __getitem3_(self,x):
-        return lambda x:x/2;
-
-print(evaluate([[0,"object"],[-1,"test"],[90],[-3],[-1,123],[98],[94]],{"object":Test()}))
-
+class PropertyValue:
+    base = None;
+    name = None;
+    def __init__(self, base,name):
+        self.base = base;
+        self.name = name;
