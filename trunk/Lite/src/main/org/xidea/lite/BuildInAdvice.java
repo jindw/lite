97364204a58652ec0c6b1c9e8b66aa17ce7b799c
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-02-14 11:46:20 +0000

    完善了一下插件机制

--- trunk/Lite/src/main/org/xidea/lite/BuildInAdvice.java
+++ trunk/Lite/src/main/org/xidea/lite/BuildInAdvice.java
@@ -1,5 +1,6 @@
 package org.xidea.lite;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -8,10 +9,15 @@ import org.apache.commons.logging.LogFactory;
 
 public class BuildInAdvice implements CompileAdvice {
 	private static Log log = LogFactory.getLog(BuildInAdvice.class);
-	private Map<String, ? extends Object> instanceMap = null;
+	private Map<String, ? extends Object> instanceMap = Collections.emptyMap();
+	private Map<String, ? extends Object> objectMap = Collections.emptyMap();
 
 	public void setInstanceMap(Map<String, ? extends Object> instanceMap) {
 		this.instanceMap = instanceMap;
+	}
+
+	public void setObjectMap(Map<String, ? extends Object> objectMap) {
+		this.objectMap = objectMap;
 	}
 
 	public void compile(Map<String, Object> gloabls, List<Object> result) {
@@ -24,6 +30,7 @@ public class BuildInAdvice implements CompileAdvice {
 				log.error("无法装载扩展：" + type, e);
 			}
 		}
+		gloabls.putAll(objectMap);
 	}
 
 }
