4ec92c61fc35d671b4a8cc782ac68ecd6c1fb40d
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-02-14 11:38:57 +0000

    完善了一下插件一通和ppt文档

--- trunk/Lite/src/main/org/xidea/lite/RuntimeAdvice.java
+++ trunk/Lite/src/main/org/xidea/lite/RuntimeAdvice.java
@@ -3,6 +3,10 @@ package org.xidea.lite;
 import java.io.Writer;
 import java.util.Map;
 
-public interface RuntimeAdvice {
+/**
+ * @author jindw
+ * 接口尚不成熟，第三方库暂勿使用
+ */
+interface RuntimeAdvice {
 	public void execute(Map<? extends Object, ? extends Object> context,Writer out);
 }
