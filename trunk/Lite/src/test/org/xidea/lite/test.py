49edc2cf0a04ce39b44e5581c6333b454c923e0d
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-02-16 06:41:33 +0000

    tython bug 易经修复了，但是js逆波兰式解析bug存在

--- trunk/Lite/src/test/org/xidea/lite/test.py
+++ trunk/Lite/src/test/org/xidea/lite/test.py
@@ -11,7 +11,8 @@ class Test:
         return lambda x:x/2;
         
 #el = "object.test(123)"
-el = [[VALUE_VAR,"object"],[VALUE_CONSTANTS,"test"],[OP_GET_PROP],[VALUE_NEW_LIST],[VALUE_CONSTANTS,123],[OP_PARAM_JOIN],[OP_INVOKE_METHOD]];
+#el = [[VALUE_VAR,"object"],[VALUE_CONSTANTS,"test"],[OP_GET_PROP],[VALUE_NEW_LIST],[VALUE_CONSTANTS,123],[OP_PARAM_JOIN],[OP_INVOKE_METHOD]];
+el = [[-1,"object"],[48,"test"],[-3],[0,123],[1,None],[81]];
 print(evaluate(el,{"object":Test()}))
 
 
