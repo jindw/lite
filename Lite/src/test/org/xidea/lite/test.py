88a861c386c4f1e9806f762bd3bf447106775f79
jindw <jindw@xidea.org>
2009-02-17 10:57:04 +0000

    python

--- Lite/src/test/org/xidea/lite/test.py
+++ Lite/src/test/org/xidea/lite/test.py
@@ -1,4 +1,5 @@
-from Expression import evaluate
+from Template import Template
+from Expression import *
 from Template import renderList
 import StringIO
 
@@ -11,14 +12,15 @@ class Test:
         return lambda x:x/2;
         
 #el = "object.test(123)"
-#el = [[VALUE_VAR,"object"],[VALUE_CONSTANTS,"test"],[OP_GET_PROP],[VALUE_NEW_LIST],[VALUE_CONSTANTS,123],[OP_PARAM_JOIN],[OP_INVOKE_METHOD]];
-el = [[-1,"object"],[48,"test"],[-3],[0,123],[1,None],[81]];
+el = [[VALUE_VAR,"object"],[VALUE_CONSTANTS,"test"],[OP_GET_PROP],[VALUE_NEW_LIST],[VALUE_CONSTANTS,123],[OP_PARAM_JOIN],[OP_INVOKE_METHOD]];
+#el = [[-1,"object"],[48,"test"],[-3],[0,123],[1,None],[81]];
 print(evaluate(el,{"object":Test()}))
 
 
 
 def test():
 	output = StringIO.StringIO();
-	Template(["12345",[0,[[0,1],[0,2],[11]]]]).render({},output)
+	#
+	Template(["3*7=",[0,[[0,3],[0,7],[OP_MUL]]]]).render({},output)
 	print output.getvalue()
 test();
