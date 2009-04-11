c0590d781d3c3d552eda0322ffc5ced89b820031
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-04-11 15:56:31 +0000

    没有加Rhino和XPath（xalan）支持

---
+++ trunk/examples/Lite4GAE/src/org/xidea/lite/gae/LoginFilter.java
@@ -0,0 +1,46 @@
+package org.xidea.lite.gae;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+        UserService userService = UserServiceFactory.getUserService();
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        String thisURL = request.getRequestURI();
+        if (request.getUserPrincipal() == null) {
+        	response.sendRedirect(userService.createLoginURL(thisURL));
+        } else {
+        	request.setAttribute("username", request.getUserPrincipal());
+        	request.setAttribute("logout", userService.createLogoutURL(thisURL));
+        	chain.doFilter(req, resp);
+        }
+
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+
+	}
+
+}
