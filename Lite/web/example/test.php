5b8807ce502279f3c97d87c9fea0927a2f4e30cf
jindw <jindw@xidea.org>
2009-04-12 16:39:01 +0000

    完善php

--- Lite/web/example/test.php
+++ Lite/web/example/test.php
@@ -1,7 +1,12 @@
 <?php
 require_once("../WEB-INF/classes/org/xidea/lite/TemplateEngine.php");
 $base = realpath("../");
-$liteCode = getLite(array("/example/layout.xhtml","/example/test.xhtml"));
+$path = "/example/test.xhtml";
+if(array_key_exists('path',$_GET)){
+	$path = $_GET['path'];
+}
+echo $path;
+$liteCode = getLite(array("/example/layout.xhtml",$path));
 $template = new Template(json_decode($liteCode));
 echo $template->render(array());
 ?>
