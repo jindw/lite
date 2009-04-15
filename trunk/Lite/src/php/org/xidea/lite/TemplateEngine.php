b4f200fe3b0d376d8b5f1e3acedc8b56892866e6
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-04-15 17:20:21 +0000

    完善php实现

--- trunk/Lite/src/php/org/xidea/lite/TemplateEngine.php
+++ trunk/Lite/src/php/org/xidea/lite/TemplateEngine.php
@@ -1,103 +1,122 @@
 <?php
 require_once("Template.php");
-$liteBase = $_SERVER["DOCUMENT_ROOT"];
-$liteService = "http://litecompiler.appspot.com"; 
 
-function liteLoad($path){
-    global $liteBase;
-    $compileDir = "$liteBase/WEB-INF/classes/phplite/";
-	if(!file_exists($compileDir)){
-		mkdir($compileDir);
+class TemplateEngine{
+	var $liteBase;
+	var $liteService; 
+	function TemplateEngine($liteBase=NULL,$liteService="http://litecompiler.appspot.com"){
+		if($liteBase == NULL){
+			$liteBase = $_SERVER["DOCUMENT_ROOT"];
+		}
+		$this->liteBase = &$liteBase;
+		$this->liteService = $liteService;
+		if(!file_exists($liteBase)){
+			echo "liteBase not found:$this->liteBase";
+			exit();
+		}
 	}
-    $liteFile = $compileDir.rawurlencode($path);
-    if(file_exists($liteFile)){
-    	$lite = json_decode(file_get_contents($liteFile));
-    	$paths = $lite[0];
-    	$liteTime = filemtime($liteFile);
-    	$fileTime = $liteTime;
-    	$i=count($paths);
-		while($i--){
-			$fileTime = max($fileTime,filemtime("$liteBase$paths[$i]"));
+	function render($path,$context=NULL){
+		$liteCode = &$this->load($path);
+		$template = new Template($liteCode);
+		if($context == NULL){
+			$context = $GLOBALS;
 		}
-		if($fileTime<=$liteTime){
-			return $lite[1];
-		}
-    }
-    $lite = liteCompile($path);
-    liteWriteCache($liteFile,json_encode($lite));
-    return $lite[1];
-}
-function liteCompile($path){
-    global $liteBase;
-	$paths = array($path);
-	$sources = array(file_get_contents(realpath("$liteBase$path")));
-	$decoratorPath = "/WEB-INF/decorators.xml";
-	$decoratorXml = file_get_contents(realpath("$liteBase$decoratorPath"));
-	if($decoratorXml){
-		array_push($sources,$decoratorXml);
-		array_push($paths,$decoratorPath);
+		$template->render($context);
 	}
-	while(true){
-		$code = liteHttpLoad($paths,$sources);
-		if(!$code){
-			continue;
+	function &load($path){
+	    $compileDir = "$this->liteBase/WEB-INF/litecached/";
+		if(!file_exists($compileDir)){
+			mkdir($compileDir,0700,true);
 		}
-		$result = json_decode($code);
-		if(!$result){
-			//echo "<hr>".$code."<hr>";
-			continue;
-		}
-		if(!is_array($result)){
-			//echo "<hr>".$code."<hr>";
-			$missed = $result->missed;
-			$retry = false;
-			foreach($missed as $path){
-				if(!in_array($path,$paths)){
-					$content = file_get_contents(realpath("$liteBase$path"));
-					
-					array_push($sources,$content);
-					array_push($paths,$path);
-					$retry = true;
-				}
+	    $liteFile = $compileDir.rawurlencode($path);
+	    if(file_exists($liteFile)){
+	    	$lite = &json_decode(file_get_contents($liteFile));
+	    	$paths = $lite[0];
+	    	$liteTime = filemtime($liteFile);
+	    	$fileTime = $liteTime;
+	    	$i=count($paths);
+			while($i--){
+				$fileTime = max($fileTime,filemtime("$this->liteBase$paths[$i]"));
 			}
-			if(!$retry){
+			if($fileTime<=$liteTime){
+				return $lite[1];
+			}
+	    }
+	    $lite = &$this->compile($path);
+	    $this->writeCache($liteFile,json_encode($lite));
+	    return $lite[1];
+	}
+	function compile($path){
+		$paths = array($path);
+		$sources = array(file_get_contents(realpath("$this->liteBase$path")));
+		$decoratorPath = "/WEB-INF/decorators.xml";
+		$decoratorXml = file_get_contents(realpath("$this->liteBase$decoratorPath"));
+		if($decoratorXml){
+			array_push($sources,$decoratorXml);
+			array_push($paths,$decoratorPath);
+		}
+		while(true){
+			$code = $this->httpLoad($paths,$sources);
+			if(!$code){
+				continue;
+			}
+			$result = json_decode($code);
+			if(!$result){
+				//echo "<hr>".$code."<hr>";
+				continue;
+			}
+			if(!is_array($result)){
+				//echo "<hr>".$code."<hr>";
+				$missed = $result->missed;
+				$retry = false;
+				foreach($missed as $path){
+					if(!in_array($path,$paths)){
+						$content = file_get_contents(realpath("$this->liteBase$path"));
+						
+						array_push($sources,$content);
+						array_push($paths,$path);
+						$retry = true;
+					}
+				}
+				if(!$retry){
+					return array($paths,$result);
+				}
+			}else{
 				return array($paths,$result);
 			}
-		}else{
-			return array($paths,$result);
 		}
 	}
+	function &httpLoad($paths,&$sources){
+		$postdata = http_build_query(
+			array(
+			    "source"=>$sources,
+			    "path"=>$paths,
+			    "compress"=>"true",
+			    "base"=>"/"
+		    )
+		);
+		//$postdata = preg_replace('/%5B(?:[0-9]+)%5D=/', '=', $postdata);
+		$opts = array('http' =>
+		    array(
+		        'method'  => 'POST',
+		        'header'  => 'Content-type: application/x-www-form-urlencoded',
+		        'content' => $postdata
+		    )
+		);
+		$context  = stream_context_create($opts);
+		return file_get_contents($this->liteService, false, $context,0,1024*1024);
+	}
+	/**
+	 * 写入文件缓存
+	 * @param $file 缓存文件路径
+	 * @param $word 缓存文件内容
+	 */
+	function writeCache ($file, &$word) {
+		$toFile = fopen($file, 'w+');
+		$lockState = flock($toFile,LOCK_EX);
+		fwrite($toFile, $word);
+		fclose($toFile);
+	}
 }
-function liteHttpLoad($paths,$sources){
-    global $liteService;
-	$postdata = http_build_query(
-		array(
-		    "source"=>$sources,
-		    "path"=>$paths,
-		    "compress"=>"true",
-		    "base"=>"/"
-	    )
-	);
-	//$postdata = preg_replace('/%5B(?:[0-9]+)%5D=/', '=', $postdata);
-	$opts = array('http' =>
-	    array(
-	        'method'  => 'POST',
-	        'header'  => 'Content-type: application/x-www-form-urlencoded',
-	        'content' => $postdata
-	    )
-	);
-	$context  = stream_context_create($opts);
-	return file_get_contents($liteService, false, $context,0,1024*1024);
-}
-/**
- * 写入文件缓存
- * @param $file 缓存文件路径
- * @param $word 缓存文件内容
- */
-function liteWriteCache ($file, $word) {
-	$toFile = fopen($file, 'w+');
-	$lockState = flock($toFile,LOCK_EX);
-	fwrite($toFile, $word);
-	fclose($toFile);
-}
+
 ?>
--- trunk/Lite/web/example/test.php
+++ trunk/Lite/web/example/test.php
@@ -1,17 +1,8 @@
 <?php
-
 require_once("../WEB-INF/classes/org/xidea/lite/TemplateEngine.php");
-$liteBase = realpath("../");
-//$liteService = "http://localhost:8080"; 
-$path = "/example/test.xhtml";
-if(array_key_exists('path',$_GET)){
-	$path = $_GET['path'];
-}
-$liteCode = liteLoad($path);
-$template = new Template($liteCode);
+$engine = new TemplateEngine(realpath("../"));
 
-
-//ob_start();
-$template->render(array("int1"=>1,"text1"=>'1'));
-//$text = ob_get_contents();ob_end_clean();echo $text;
+$int1 = 1;
+$text1 = '1';
+$engine->render("/example/test.xhtml");
 ?>
