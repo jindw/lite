76fb36649f2430d0ed9c8a855d5e50b66ca3811a
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-04-20 18:33:01 +0000

    完善python测试

--- trunk/Lite/web/example/test.py
+++ trunk/Lite/web/example/test.py
@@ -7,19 +7,15 @@ from lite import TemplateEngine
 #from TemplateEngine import TemplateEngine
 from mod_python import apache
 
-engine = TemplateEngine("D:/workspace/Lite/web/",None);
+engine = TemplateEngine("D:/workspace/Lite/web/");
 
 def handler(req):
     req.content_type = "text/html";
+    #数据模型
     context = {
     	"int1":1,
     	"text1":'1'
     }
+    #渲染模板
     engine.render("/example/test.xhtml",context,req);
     return apache.OK
-
-    #Template(["constants:123=",[EL_TYPE,[[VALUE_CONSTANTS,123]]],"\r\n"]).render({},output)
-    #Template(["constants:v1=",[EL_TYPE,[[VALUE_VAR,"v1"]]],"\r\n"]).render({"v1":1},output)
-    #test for:<c:for var="item" items="${[1,2,3,4]}" status="status">${item+status.index}</c:for>
-    #Template(["test for:",[FOR_TYPE,[[EL_TYPE,  [[VALUE_VAR,"item"],[VALUE_VAR,"status"],[OP_STATIC_GET_PROP,"index"],[OP_ADD]]  ]],"item",[[VALUE_CONSTANTS,[1,2,3,4]]],"status"]]).render({},output);
-    #output.close();
