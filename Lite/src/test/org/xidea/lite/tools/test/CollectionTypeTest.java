3910445e19ea4d8408d2af8ac5ecfb7047ffb1c8
jindw <jindw@xidea.org>
2009-07-05 16:55:39 +0000

    准备添范型集合支持

--- Lite/src/test/org/xidea/lite/tools/test/CollectionTypeTest.java
+++ Lite/src/test/org/xidea/lite/tools/test/CollectionTypeTest.java
@@ -1,23 +1,19 @@
 package org.xidea.lite.tools.test;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.junit.Test;
+import org.xidea.el.impl.ReflectUtil;
+
 
 public class CollectionTypeTest {
-	public ArrayList<Collection<Double>> stringCollection = new ArrayList<Collection<Double>>();
-	public StringList<Integer> stringCollection2 = new StringList<Integer>();
+	public ArrayList<? extends Collection<Double>> stringCollection = new ArrayList<Collection<Double>>();
+	public StringList<? extends Integer> stringCollection2 = new StringList<Integer>();
+	public StringList2<Integer,? extends String> stringCollection3 = new StringList2<Integer,String>();
 
 	public Collection<String> stringMap = new ArrayList<String>();
 	public StringMap<String, Boolean> stringMap2 = new StringMap<String, Boolean>();
@@ -27,11 +23,14 @@ public class CollectionTypeTest {
 		private static final long serialVersionUID = 1L;
 	};
 
+	public static class NumberList<T> extends ArrayList<Number> {
+		private static final long serialVersionUID = 1L;
+	};
 	public static class StringList2<K, T> extends StringList<K> {
 		private static final long serialVersionUID = 1L;
 	};
 
-	public static class StringList3<K3, T,K> extends StringList2<K3, T> {
+	public static class StringList3<K3, T, K> extends StringList2<K3, T> {
 		private static final long serialVersionUID = 1L;
 	};
 
@@ -39,55 +38,23 @@ public class CollectionTypeTest {
 		private static final long serialVersionUID = 1L;
 	};
 
-	public Type getType(Class<?> clazz, Type pt) {
-		if (List.class.equals(clazz) || Set.class.equals(clazz)
-				|| Collection.class.equals(clazz)) {
-			if (pt instanceof ParameterizedType) {
-				return ((ParameterizedType) pt)
-						.getActualTypeArguments()[0];
-			}
-			return Object.class;
-		}
-		Class<?>[] ifs = clazz.getInterfaces();
-		Type[] pis = clazz.getGenericInterfaces();
-		for (int i = 0; i < ifs.length; i++) {
-			Class<?> ifc = ifs[i];
-			if (Collection.class.isAssignableFrom(ifc)) {
-				return getTureType(getType(ifc, pis[i]),pt);
-			}
-		}
-		Class<?> type2 = clazz.getSuperclass();
-		if (type2 != null) {
-			if (Collection.class.isAssignableFrom(type2)) {
-				return getType(type2, clazz.getGenericSuperclass());
-			}
-		}
-		return null;
-	}
 
-	private Type getTureType(Type type, Type actualMap) {
-		if (type instanceof Class) {
-			return type;
-		}else if (type instanceof TypeVariable) {
-			TypeVariable tv = (TypeVariable)type;
-			if(actualMap instanceof ParameterizedType){
-				ParameterizedType pt = (ParameterizedType)actualMap;
-				Type[] actualTypes = pt.getActualTypeArguments();
-				System.out.println("$"+Arrays.asList(actualTypes[0]));
-			}
-			return tv;
-		}
-		return type;
-	}
 	@Test
 	public void testGetListType() throws Exception {
 		try {
-			Field field = this.getClass().getField("stringCollection");
-			Class<?> type = field.getType();
-			System.out.println(":"+getType(type, field.getGenericType()));
+			testField("stringCollection");
+			testField("stringCollection2");
+			testField("stringCollection3");
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void testField(String key) throws NoSuchFieldException {
+		Field field = this.getClass().getField(key);
+		Class<?> type = field.getType();
+		Type gtype = field.getGenericType();
+		System.out.println(":" + ReflectUtil.getValueType(gtype));
 	}
 
 	@Test
