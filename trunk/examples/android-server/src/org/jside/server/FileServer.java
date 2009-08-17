87eaa78eaec27d9968344a92fec9a1a52ad58406
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-08-17 16:07:42 +0000

    by dy

--- trunk/examples/android-server/src/org/jside/server/FileServer.java
+++ trunk/examples/android-server/src/org/jside/server/FileServer.java
@@ -1,42 +1,56 @@
 package org.jside.server;
 
 import java.io.File;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.util.HashMap;
 
+import org.jside.server.web.TemplateEngine;
 import org.jside.webserver.MutiThreadWebServer;
 import org.jside.webserver.RequestContext;
 
+public class FileServer extends MutiThreadWebServer {
 
-public class FileServer extends MutiThreadWebServer{
+	private TemplateEngine engine = new TemplateEngine();
 
-	public FileServer(){
+	public FileServer() {
 		super("file:///");
 	}
 
 	@Override
 	public void processRequest(RequestContext context) throws Exception {
-		URL res = new URL(webBase, context.getRequestURI()
-				.substring(1));
-		if(res.getProtocol().equals("file")){
+		URL res = new URL(webBase, context.getRequestURI().substring(1));
+		if (res.getProtocol().equals("file")) {
 			File file = getFile(res);
-			if(file.isDirectory()){
-				
-			}else{
+			if (file.isDirectory()) {
+				OutputStreamWriter out = new OutputStreamWriter(context
+						.getOutputStream(), "UTF-8");
+				File[] list = file.listFiles();
+				HashMap<String, Object> data = new HashMap<String, Object>();
+				data.put("path", file.getAbsoluteFile());
+				data.put("fileList", list);
+				context.setContentType("text/html;charset=utf-8");
+				try {
+					engine.render("dir.xhtml", data, out);
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			} else {
 				super.processRequest(context);
 			}
-		}else{
+		} else {
 			super.processRequest(context);
 		}
 	}
 
 	private File getFile(URL res) {
 		try {
-			return  new File(URLDecoder.decode(res.getFile(),"UTF-8"));
+			return new File(URLDecoder.decode(res.getFile(), "UTF-8"));
 		} catch (UnsupportedEncodingException e) {
 			return null;
 		}
 	}
-	
+
 }
