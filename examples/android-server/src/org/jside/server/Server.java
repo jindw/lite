5b41151deeeb406622b116ae807dd1d0c1a68e47
jindw <jindw@xidea.org>
2009-08-17 16:07:42 +0000

    by dy

--- examples/android-server/src/org/jside/server/Server.java
+++ examples/android-server/src/org/jside/server/Server.java
@@ -1,19 +1,25 @@
 package org.jside.server;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class Server extends Activity{
+	static Server instance;
 	/** Called when the activity is first created. */
 	private FileServer ws = new FileServer();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		instance = this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		Button exit = (Button) super.findViewById(R.id.exit);
@@ -25,22 +31,61 @@ public class Server extends Activity{
 		});
 		this.start();
 	}
+	public static void alert(String msg){
+		AlertDialog.Builder builder = new AlertDialog.Builder(instance);
+		builder.setMessage(msg)
+		       .setCancelable(true)
+		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	public boolean prompt(String msg){
+		final boolean[] rtv = new boolean[]{false};
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(msg)
+		       .setCancelable(true)
+		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   rtv[0] = true;
+		        	   dialog.cancel();
+		           }
+		       })
+		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   rtv[0] = false; 
+		        	   dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+
+		return rtv[0];
+	}
 
 	public void start() {
 		EditText link = (EditText) this.findViewById(R.id.link);
 		try {
 			ws.start();
-			WifiManager wm = (WifiManager) this.getSystemService(Activity.WIFI_SERVICE);
 			String ip = "127.0.0.1";
-			if (wm != null && wm.getConnectionInfo() !=null) {
-				int i = wm.getConnectionInfo().getIpAddress();
-				ip = (i >> 24) + "." + (0xFF & (i >> 16)) + '.'
-						+ (0xFF & (i >> 8)) + '.' + (0xFF & i);
+			WifiManager wf = (WifiManager)this.getSystemService(WIFI_SERVICE);
+			if(wf!=null){
+				DhcpInfo dh = wf.getDhcpInfo();
+				if(dh!=null){
+					ip = getIp(dh.ipAddress);
+				}
 			}
 			link.setText("http://" + ip + ":" + ws.getPort());
 		} catch (Throwable e) {
-			String msg = e.getMessage();
-			link.setText(msg.replaceAll("[\r\n]", ""));
+			alert(Log.getStackTraceString(e));
 		}
+	}
+	private String getIp(int i) {
+		return (0xFF & i) + "." + (0xFF & (i >> 8)) + '.'
+		+ (0xFF & (i >> 16)) + '.' + (i >> 24);
 	}
 }
