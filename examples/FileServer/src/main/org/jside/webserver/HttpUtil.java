7cebe33125ca96b388f70303f0fe496383a547ef
jindw <jindw@xidea.org>
2009-10-15 10:39:16 +0000

    by dawei

--- examples/FileServer/src/main/org/jside/webserver/HttpUtil.java
+++ examples/FileServer/src/main/org/jside/webserver/HttpUtil.java
@@ -3,7 +3,6 @@ package org.jside.webserver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
