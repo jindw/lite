48ccd8a446e25ad21532b5fdfcae165fa0f7babb
jindw <jindw@xidea.org>
2009-11-08 14:42:38 +0000

    优化ParseContext，ParseChain有待进一步优化

--- Lite/src/main/org/xidea/lite/parser/impl/ResourceContextImpl.java
+++ Lite/src/main/org/xidea/lite/parser/impl/ResourceContextImpl.java
@@ -3,23 +3,50 @@ package org.xidea.lite.parser.impl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
+import java.util.regex.Pattern;
 
 import org.xidea.lite.parser.ResourceContext;
 
 public class ResourceContextImpl implements ResourceContext {
+	private static Pattern URI_ROOT = Pattern.compile("^[\\w]+\\:");
+	private static Pattern URI_CLEAR = Pattern
+			.compile("\\/\\.\\/|\\/[^\\/]+\\/\\.\\.\\/");
 	protected final URI base;
 
 	public ResourceContextImpl(URI base) {
 		this.base = base;
 	}
+
 	public URI createURI(String path, URI parentURI) {
 		try {
-			if (path.startsWith("/")) {
-				return new URL(this.base.toURL(), path.substring(1)).toURI();
+			if (URI_ROOT.matcher(path).find()) {
+				return URI.create(path);
 			} else {
 				URI parent = parentURI != null ? parentURI : this.base;
-				return new URL(parent.toURL(), path).toURI();
+				if (path.startsWith("/")) {
+					if (parent.toString().startsWith(base.toString())) {
+						path = (base.getPath() + path).replaceFirst(
+								"^\\/|\\/\\/", "");
+					}
+				} else {
+					String dir = parent.getPath();
+					if (dir.endsWith("/")) {
+						dir = dir + "a";
+					}
+					path = dir + "/../" + path;
+					int length = -1;
+					while (true) {
+						path = URI_CLEAR.matcher(path).replaceFirst("/");
+						int length2 = path.length();
+						if (length2 == length) {
+							break;
+						} else {
+							length = length2;
+						}
+					}
+				}
+				return new URI(parent.getScheme(), parent.getUserInfo(), parent
+						.getHost(), parent.getPort(), path, null, null);
 			}
 
 		} catch (Exception e) {
