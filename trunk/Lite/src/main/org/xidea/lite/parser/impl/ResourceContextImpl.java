1d7ac3afc03f747c95c4095977922ddeb92ea603
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-11-08 08:36:14 +0000

    classpath 直接支持

--- trunk/Lite/src/main/org/xidea/lite/parser/impl/ResourceContextImpl.java
+++ trunk/Lite/src/main/org/xidea/lite/parser/impl/ResourceContextImpl.java
@@ -62,7 +62,8 @@ public class ResourceContextImpl implements ResourceContext {
 			if (path.startsWith("/")) {
 				return new URL(this.base.toURL(), path.substring(1)).toURI();
 			} else {
-				return new URL(parentURI != null ? parentURI.toURL() : this.base.toURL(), path).toURI();
+				URI parent = parentURI != null ? parentURI : this.base;
+				return new URL(parent.toURL(), path).toURI();
 			}
 
 		} catch (Exception e) {
@@ -70,9 +71,23 @@ public class ResourceContextImpl implements ResourceContext {
 		}
 	}
 
-	public InputStream openInputStream(URI url) {
+	public InputStream openInputStream(URI uri) {
 		try {
-			return url.toURL().openStream();
+			if ("classpath".equalsIgnoreCase(uri.getScheme())) {
+				ClassLoader cl = this.getClass().getClassLoader();
+				String path = uri.getPath();
+				InputStream in = cl.getResourceAsStream(path);
+				if (in == null) {
+					ClassLoader cl2 = Thread.currentThread()
+							.getContextClassLoader();
+					if (cl2 != null) {
+						in = cl2.getResourceAsStream(path);
+					}
+				}
+				return in;
+			} else {
+				return uri.toURL().openStream();
+			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
