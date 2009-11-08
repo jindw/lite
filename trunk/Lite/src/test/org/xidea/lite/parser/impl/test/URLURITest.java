2823138a55d368f913382dccc2a609f69795d29f
jindwcn <jindwcn@c24ab3aa-bf55-11dd-871e-457e4afae058>
2009-11-08 14:42:38 +0000

    优化ParseContext，ParseChain有待进一步优化

--- trunk/Lite/src/test/org/xidea/lite/parser/impl/test/URLURITest.java
+++ trunk/Lite/src/test/org/xidea/lite/parser/impl/test/URLURITest.java
@@ -2,16 +2,45 @@ package org.xidea.lite.parser.impl.test;
 
 
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.xidea.el.impl.ReflectUtil;
+import org.xidea.lite.parser.impl.ResourceContextImpl;
 
 
 public class URLURITest {
 
 	@Before
 	public void setUp() throws Exception {
+	}
+	@Test
+	public void testResourceContext() throws Exception{
+		String base = "http://lh:8080/test";
+		ResourceContextImpl rc = new ResourceContextImpl(new URI(base));
+		Assert.assertEquals(base+".xml", rc.createURI("test.xml", null).toString());
+		Assert.assertEquals(base+".xml", rc.createURI("./test.xml", null).toString());
+		
+		base = "http://lh:8080/test/aa/bb/";
+		rc = new ResourceContextImpl(new URI(base));
+		System.out.println(ReflectUtil.map(new URI("classpath:///aa/bb")));
+		Assert.assertEquals(base+"test.xml", rc.createURI("test.xml", null).toString());
+		Assert.assertEquals("http://lh:8080/test/test.xml", rc.createURI("../../test.xml", null).toString());
+		Assert.assertEquals("http://lh:8080/test/test.xml", rc.createURI("../.././test.xml", null).toString());
+		Assert.assertEquals("http://lh:8080/test/test.xml", rc.createURI(".././../test.xml", null).toString());
+		Assert.assertEquals("http://lh:8080/test/test.xml", rc.createURI("../././../test.xml", null).toString());
+		
+	
+	}
+	@Test
+	public void testURIRelative() throws Exception{
+		String t = "classpath:aa/bb/cc/../../dd";
+		System.out.println(new URI(t));
+		t = t.replaceFirst("([^\\/]+\\/\\.)?\\.\\/", "");
+		System.out.println(new URI(t));
 	}
 	@Test
 	public void testURIFile(){
