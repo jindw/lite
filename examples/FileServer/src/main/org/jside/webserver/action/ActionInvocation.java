5847e5f5f74f6fed78ae3624bc57cb19db375522
jindw <jindw@xidea.org>
2010-08-01 18:19:05 +0000

    Lite2Alpha2

--- examples/FileServer/src/main/org/jside/webserver/action/ActionInvocation.java
+++
@@ -1,9 +0,0 @@
-package org.jside.webserver.action;
-
-import org.jside.webserver.RequestContext;
-
-public interface ActionInvocation {
-	public boolean match(String uri);
-	public void execute(RequestContext context) throws Exception;
-}
-
